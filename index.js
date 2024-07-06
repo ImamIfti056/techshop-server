90c6f52564738e9732c003491b87772b936f46f9
ImamIfti056 <imamul.ifti@gmail.com>
2024-07-06 20:06:44 +0600

    cart pending

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+const port = 5000;
 const cors = require('cors')
 require('dotenv').config()
 
@@ -9,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kahzgwg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,10 +28,72 @@ async function run() {
     await client.connect();
 
     const componentsCollection = client.db("techshop").collection('components');
-    
-    app.get('/components', async(req, res) => {
-        const result = await componentsCollection.find().toArray();
-        res.send(result)
+    const cartCollection = client.db("techshop").collection('cart');
+    const usersCollection = client.db("techshop").collection('users');
+
+    app.get('/components', async (req, res) => {
+      const result = await componentsCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    // users---------------------------
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: 'User already exists', insertedId: null})
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    //make admin
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    // cart---------------------------------
+    app.get('/cart', async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    // cart items
+    app.post('/cart', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result)
+    })
+
+    app.delete('/cart/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
@@ -46,12 +109,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('server running')
-})
-app.get('/ss', (req, res) => {
-    res.send('ss ss server running')
+  res.send('server running')
 })
 
 app.listen(port, () => {
-    console.log('listtttening', {port})
+  console.log('listtttening', { port })
 })
